e657bdb0f571c040cbf224dc078b266a6b8fae49
tybirk <pet@accuranker.com>
2026-01-06 22:50:18 +0100

    deny list

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -37,6 +37,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        navigateFallbackDenylist: [/^\/admin/, /^\/api/, /^\/media/],
       },
     }),
   ],
